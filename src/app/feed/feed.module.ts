62e9917bf4d690c5ac5e51790ade1ca258838a94
twanou <twan472@hotmail.com>
2023-11-23 20:14:20 -0500

    wip5

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -6,10 +6,13 @@ import { SubjectCardComponent } from './subject-card/subject-card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { SbCommonModule } from '../sb-common/sb-common.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [DeputyFeedComponent, SubjectCardComponent],
-  imports: [CommonModule, ApiModule, MatCardModule, MatExpansionModule, SbCommonModule],
+  imports: [CommonModule, ApiModule, MatCardModule, MatExpansionModule, MatButtonModule, MatTooltipModule, MatIconModule, SbCommonModule],
   exports: [DeputyFeedComponent],
 })
 export class FeedModule {}
