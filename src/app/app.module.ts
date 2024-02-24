d8348369f0d0c92cf7d002aa3011296db1e9792c
twanou <twan472@hotmail.com>
2024-02-23 21:39:29 -0500

    Ajout de ngx-translate et autres amélioratons en tous genres.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,25 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { SidenavModule } from './sidenav/sidenav.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     AppRoutingModule,
     BrowserAnimationsModule,
     FeedModule,
--- src/app/sb-common/sb-common.module.ts
+++ src/app/sb-common/sb-common.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslateModule } from '@ngx-translate/core';
 import { AvatarComponent } from './avatar/avatar.component';
 import { PageTemplateComponent } from './page-template/page-template.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [SpinnerComponent, AvatarComponent, PageTemplateComponent],
   imports: [CommonModule, MatProgressSpinnerModule],
-  exports: [SpinnerComponent, AvatarComponent, PageTemplateComponent],
+  exports: [SpinnerComponent, AvatarComponent, PageTemplateComponent, TranslateModule],
 })
 export class SbCommonModule {}
