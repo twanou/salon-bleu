7d8715bf625583faeed7845c0ee1038106aa0679
twanou <twan472@hotmail.com>
2024-03-30 22:09:04 -0400

    hdghfgdhdfg

--- src/app/sb-common/pipes/sbdate.pipe.ts
+++ src/app/sb-common/pipes/sbdate.pipe.ts
@@ -20,19 +20,22 @@ export class SbdatePipe implements PipeTransform {
     'décembre',
   ];
 
-  transform(...values: string[]): string {
+  transform(value: string | string[]): string {
+    let values: string[] = [];
+    values = values.concat(value);
     const dateBuilder: string[] = [];
     for (let i = 0; i < values.length; ++i) {
-      const date = new Date(values[i]);
+      const currentDate = new Date(values[i]);
       const nextDate = i + 1 < values.length ? new Date(values[i + 1]) : null;
-      dateBuilder.push(this.getDate(date));
-      if (nextDate && this.getMonth(date) === this.getMonth(nextDate)) {
-        dateBuilder.push(', ');
-      } else if (nextDate) {
-        dateBuilder.push(` ${this.getMonth(date)}, `);
-      } else {
-        dateBuilder.push(` ${this.getMonth(date)} ${date.getUTCFullYear()}`);
+      const isNextLastDate = i === values.length - 2;
+
+      dateBuilder.push(this.getDate(currentDate));
+      if (nextDate && !this.sameMonth(currentDate, nextDate) && this.sameYear(currentDate, nextDate)) {
+        dateBuilder.push(` ${this.getMonth(currentDate)}`);
+      } else if (!nextDate || !this.sameYear(currentDate, nextDate)) {
+        dateBuilder.push(` ${this.getMonth(currentDate)} ${this.getYear(currentDate)}`);
       }
+      dateBuilder.push(this.getSeparator(nextDate, isNextLastDate));
     }
     return dateBuilder.join('');
   }
@@ -43,5 +46,27 @@ export class SbdatePipe implements PipeTransform {
 
   private getMonth(date: Date): string {
     return `${this.months[date.getUTCMonth()]}`;
+  }
+
+  private getYear(date: Date): string {
+    return `${date.getUTCFullYear()}`;
+  }
+
+  private sameMonth(dateA: Date, dateB: Date): boolean {
+    return this.getMonth(dateA) === this.getMonth(dateB);
+  }
+
+  private sameYear(dateA: Date, dateB: Date): boolean {
+    return this.getYear(dateA) === this.getYear(dateB);
+  }
+
+  private getSeparator(nextDate: Date | null, isNextLastDate: boolean) {
+    if (isNextLastDate) {
+      return ' et ';
+    } else if (nextDate) {
+      return ', ';
+    } else {
+      return '';
+    }
   }
 }
